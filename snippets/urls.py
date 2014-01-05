d40cc1e56c661467c58ebe4590ae3b7f1679f891
Paul E <pebre79@hotmail.com>
2014-01-05 01:54:09 -0600

    finish part2

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('snippets.views',
     url(r'^snippets/$', 'snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
 )
+
+# Enable the ability to pass serializization patterns
+# e.g.  /snippets/.json will return JSON
+# or /snippets/.api will show an API view
+urlpatterns += format_suffix_patterns(urlpatterns)
