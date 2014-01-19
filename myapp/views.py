7151e3e1b12695da662b83e6349c1c5b15edbecb
Paul E <pebre79@hotmail.com>
2014-01-18 21:47:00 -0600

    remove extraneous files

--- myapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, get_object_or_404, redirect, HttpResponse, render_to_response, HttpResponseRedirect
-from django.core.urlresolvers import reverse
-
-def index(request):
-    return HttpResponse('Hello World')
